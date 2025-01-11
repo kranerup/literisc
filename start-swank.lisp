872b093666e0060f0e8bcd55389725fcab89995b
Kenny Ranerup <kranerup@gmail.com>
2025-01-11 13:27:01 +0100

    updated slimv startup script

--- start-swank.lisp
+++
@@ -1,4 +0,0 @@
-(ql:quickload '(:swank) :silent t)
-(swank:create-server :port 5556 :dont-close t)
-(loop (sleep 1))
-;;; let g:swank_port=5556
---
+++ start.sh
@@ -0,0 +1,1 @@
+SWANK_PORT=5556 sbcl --load $HOME/.config/nvim/bundle/slimv/slime/start-swank.lisp
