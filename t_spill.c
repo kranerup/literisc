930bbd76fe42ce2add600d9e211a709756742fab
installer <installer@gmail.com>
2026-02-22 10:44:05 +0100

    updated register spill test with volatile

--- t_spill.c
+++ t_spill.c
@@ -1,10 +1,10 @@
+volatile    int a = 1, b = 2, c = 3, d = 4;
+volatile    int e = 5, f = 6, g = 7, h = 8;
+volatile    int i = 9, j = 10, k = 11, l = 12;
+volatile    int m = 13, n = 14, o = 15, p = 16;
+volatile    int q = 17, r = 18;
 int main() {
     // This function uses more than 16 variables to force spilling.
-    int a = 1, b = 2, c = 3, d = 4;
-    int e = 5, f = 6, g = 7, h = 8;
-    int i = 9, j = 10, k = 11, l = 12;
-    int m = 13, n = 14, o = 15, p = 16;
-    int q = 17, r = 18;
 
     int result = a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r;
 
--- t_spill_final.c
+++ t_spill_final.c
@@ -1,40 +1,40 @@
 // Final attempt to force a spill.
+volatile    int v0 = 1;
+volatile    int v1 = 2;
+volatile    int v2 = 3;
+volatile    int v3 = 4;
+volatile    int v4 = 5;
+volatile    int v5 = 6;
+volatile    int v6 = 7;
+volatile    int v7 = 8;
+volatile    int v8 = 9;
+volatile    int v9 = 10;
+volatile    int v10 = 11;
+volatile    int v11 = 12;
+volatile    int v12 = 13;
+volatile    int v13 = 14;
+volatile    int v14 = 15;
+volatile    int v15 = 16;
+volatile    int v16 = 17;
+volatile    int v17 = 18;
+volatile    int v18 = 19;
+volatile    int v19 = 20;
 // The idea is to make many variables live for the entire duration of the function.
 int main() {
-    int v0 = 1;
-    int v1 = 2;
-    int v2 = 3;
-    int v3 = 4;
-    int v4 = 5;
-    int v5 = 6;
-    int v6 = 7;
-    int v7 = 8;
-    int v8 = 9;
-    int v9 = 10;
-    int v10 = 11;
-    int v11 = 12;
-    int v12 = 13;
-    int v13 = 14;
-    int v14 = 15;
-    int v15 = 16;
-    int v16 = 17;
-    int v17 = 18;
-    int v18 = 19;
-    int v19 = 20;
 
     // By mixing variables from the beginning and end of the declaration list,
     // we force their live intervals to span the whole function.
-    int r0 = v0 + v19;
-    int r1 = v1 + v18;
-    int r2 = v2 + v17;
-    int r3 = v3 + v16;
-    int r4 = v4 + v15;
-    int r5 = v5 + v14;
-    int r6 = v6 + v13;
-    int r7 = v7 + v12;
-    int r8 = v8 + v11;
-    int r9 = v9 + v10;
+    int x0 = v0 + v19;
+    int x1 = v1 + v18;
+    int x2 = v2 + v17;
+    int x3 = v3 + v16;
+    int x4 = v4 + v15;
+    int x5 = v5 + v14;
+    int x6 = v6 + v13;
+    int x7 = v7 + v12;
+    int x8 = v8 + v11;
+    int x9 = v9 + v10;
 
-    int final_result = r0 + r1 + r2 + r3 + r4 + r5 + r6 + r7 + r8 + r9;
+    int final_result = x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9;
     return final_result;
 }
